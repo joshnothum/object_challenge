ae922cf70ff85003fa8019b7134fc5091b4362c3
Josh Nothum <joshnothum@gmail.com>
2017-10-03 23:43:23 -0500

    can't source js file properly

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+console.log('js source');
+
+var locations = []; // this array will hold your objects
+
+// object constructor
+function NewState(nameIn, revenueIn, locationsIn){
+  this.name = nameIn;
+  this.revenue = revenueIn;
+  this.location = locationsIn;
+  this.perChainAverage = perChainAverage;
+
+  function averageRevenue(pizzaArray){
+    var perChainAverage = this.revenue/this.locations;
+    return  perChainAverage;
+  }
+}
+var missouri = new State("MO",30000,12);
 
 // object instances
 
